7d637b7a1c1a4b1108263386badd284204880262
RodolfoBohn <ensino.rodolfofbohn@gmail.com>
2023-05-07 15:35:52 -0300

    Adicionando biblioteca keen slides

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
 import { HomeWrapper, Product } from "@/styles/home";
+import { useKeenSlider } from 'keen-slider/react'
 import Image from "next/image";
 
 import camiseta1 from '../assets/camisetas/1.png'
 import camiseta2 from '../assets/camisetas/2.png'
 import camiseta3 from '../assets/camisetas/3.png'
 
+import 'keen-slider/keen-slider.min.css'
+
 export default function Home() {
+  const [ slidesref ] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
   return (
     <>
-      <HomeWrapper>
-        <Product>
+      <HomeWrapper ref={slidesref} className="keen-slider">
+        <Product className='keen-slider__slide'>
           <Image src={camiseta1} width={520} height={480} alt="" />
 
           <footer>
@@ -18,11 +27,29 @@ export default function Home() {
           </footer>
         </Product>
 
-        <Product>
+        <Product className='keen-slider__slide'>
           <Image src={camiseta2} width={520} height={480} alt="" />
 
           <footer>
             <strong>Camiseta Y</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+
+        <Product className='keen-slider__slide'>
+          <Image src={camiseta3} width={520} height={480} alt="" />
+
+          <footer>
+            <strong>Camiseta Z</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+
+        <Product className='keen-slider__slide'>
+          <Image src={camiseta3} width={520} height={480} alt="" />
+
+          <footer>
+            <strong>Camiseta Z</strong>
             <span>R$ 79,90</span>
           </footer>
         </Product>
